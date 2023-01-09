67be291df109a1070b2c0ecf7b1cbe8ce22d6865
Pablo-Severino <pabloseverino40@gmail.com>
2023-01-09 12:29:44 -0300

    feat: using useEffect, useState and useMemo

---
+++ Aula07/src/app/pages/login/Login.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react"
+
+export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    // if (window.confirm("Você é homem ?")) {
+    //   console.log("Homem");
+    // } else {
+    //   console.log("Mulher");
+    // }
+  }, []);
+
+  useEffect(() => {
+    console.log(email);
+  }, [email]);
+  
+  useEffect(() => {
+    console.log(password);
+  }, [password]);
+
+  const handleEntrar = () => {
+    console.log(email);
+    console.log(password);
+  }
+
+  return (
+    <div>
+      <form>
+        <label>
+          <span>Email</span>
+          <input
+            type="email"
+            value={email}
+            onChange={e => 
+              setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha</span>
+          <input
+            type="password"
+            value={password}
+            onChange={e => 
+              setPassword(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={handleEntrar}>
+          Entrar
+        </button>
+      </form>
+    </div>
+  )
+}
