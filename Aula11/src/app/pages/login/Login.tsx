e8c0cb4576c2172e9e43da9cc96144be106466e8
Pablo-Severino <pabloseverino40@gmail.com>
2023-01-09 22:54:33 -0300

    feat: using useRef

---
+++ Aula11/src/app/pages/login/Login.tsx
@@ -0,0 +1,46 @@
+import { useCallback, useMemo, useRef, useState } from "react"
+
+export const Login = () => {
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const emailLength = useMemo(() => {
+    return email.length * 1000
+  }, [email.length]);
+
+  const handleEntrar = useCallback(() => {
+    console.log(email);
+    console.log(password);
+  }, [email, password]);
+
+  return (
+    <div>
+      <form>
+        <p>Quantidade de caracteres no email: {emailLength}</p>
+        <label>
+          <span>Email</span>
+          <input
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            onKeyDown={e => e.key === "Enter" ? inputPasswordRef.current?.focus() : undefined}
+          />
+        </label>
+        <label>
+          <span>Senha</span>
+          <input
+            type="password"
+            value={password}
+            ref={inputPasswordRef}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={handleEntrar}>
+          Entrar
+        </button>
+      </form>
+    </div>
+  )
+}
